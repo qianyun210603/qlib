f8757eaffe84a2b20fcd4926ba7c04164dffbf26
BookSword <qianyun210603@hotmail.com>
2023-02-24 15:56:16 +0800

    fix cb strategies

--- qlib/contrib/strategy/cb_signal_strategy.py
+++ qlib/contrib/strategy/cb_signal_strategy.py
@@ -74,9 +74,20 @@ class TopkKeepnDropoutCBStrategy(TopkKeepnDropoutStrategy):
             pred_score, current_stock_list, trade_start_time, trade_end_time
         )
 
-        pred_df = pred_score.sort_values(ascending=False, kind="stable").to_frame()
+        pred_df = pred_score.to_frame(name="score")
         pred_df["current_hold"] = pred_df.index.isin(current_stock_list)
-        pred_df["cum_current_hold"] = pred_df["current_hold"].cumsum()
+
+        pred_df["call_announced"] = (
+            pred_df.apply(
+                lambda x: self.trade_exchange.quote.get_data(
+                    x.name, trade_start_time, trade_end_time, field="$call_announced", method="ts_data_last"
+                ),
+                axis=1,
+            ).fillna(1)
+            > 0.5
+        )
+
+
         pred_df["tradestatusflag"] = (
             pred_df.apply(
                 lambda x: 0
@@ -89,10 +100,15 @@ class TopkKeepnDropoutCBStrategy(TopkKeepnDropoutStrategy):
             if self.only_tradable
             else 0
         )
+        pred_df.sort_values(
+            by=["tradestatusflag", "score", "current_hold"], ascending=False, inplace=True, kind="stable"
+        )
 
         # sell all sellable holdings which annouced force redemption
         sell = pred_df[pred_df.current_hold & (pred_df.tradestatusflag == 0) & pred_df.call_announced].index
         # drop items annouced force redemption
+        pred_df = pred_df[~pred_df.call_announced]
+
         pred_df["rank"] = (pred_df["current_hold"] | (0 == pred_df["tradestatusflag"])).cumsum()
         pred_df["cum_current_hold"] = pred_df["current_hold"].cumsum()
 
@@ -100,13 +116,11 @@ class TopkKeepnDropoutCBStrategy(TopkKeepnDropoutStrategy):
 
         sell = sell.union(removed_from_population)
 
-        pred_df["keep"] = pred_df["current_hold"] & (
-            (pred_df["rank"] <= self.keepn)
-            & (
+        pred_df["keep"] = pred_df["current_hold"] & (pred_df["rank"] <= self.keepn) & (
                 (pred_df["cum_current_hold"] <= len(current_stock_list) - additional_n_drop)
                 | (pred_df["rank"] <= self.topk)
             )
-        )
+
         if self.only_positive_score:
             pred_df["keep"] = pred_df["keep"] & (pred_df["score"] >= 0.0)
             pred_df = pred_df[(pred_df["score"] >= 0.0) | pred_df.current_hold]

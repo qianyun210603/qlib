4a2fef905a02ebc17d5d69ff95ee87bae330ff35
booksword <qianyun210603@hotmail.com>
2023-05-30 19:34:05 +0800

    future min calendar

--- scripts/data_collector/future_calendar_collector.py
+++ scripts/data_collector/future_calendar_collector.py
@@ -13,12 +13,14 @@ import fire
 import numpy as np
 import pandas as pd
 from loguru import logger
+from utils import generate_minutes_calendar_from_daily
 
 
 class CollectorFutureCalendar:
     calendar_format = "%Y-%m-%d"
+    intraday_format = "%Y-%m-%d %H:%M:%S"
 
-    def __init__(self, qlib_dir: Union[str, Path], start_date: str = None, end_date: str = None, freq='day'):
+    def __init__(self, qlib_dir: Union[str, Path], start_date: str = None, end_date: str = None, freq="day"):
         """
 
         Parameters
@@ -30,6 +32,7 @@ class CollectorFutureCalendar:
         end_date
             end date
         """
+        assert freq in {"day", "1min"}, "freq must be either `day` or `1min`."
         self.freq = freq
         self.qlib_dir = Path(qlib_dir).expanduser().absolute()
         self.calendar_path = self.qlib_dir.joinpath(f"calendars/{freq}.txt")
@@ -49,16 +52,19 @@ class CollectorFutureCalendar:
         calendar_df["date"] = pd.to_datetime(calendar_df["date"])
         return calendar_df["date"].to_list()
 
-    def _format_datetime(self, datetime_d: [str, pd.Timestamp]):
+    def _format_datetime(self, datetime_d: [str, pd.Timestamp], freq="day"):
         datetime_d = pd.Timestamp(datetime_d)
-        return datetime_d.strftime(self.calendar_format)
+        format_str = self.calendar_format if freq == "day" else self.intraday_format
+        return datetime_d.strftime(format_str)
 
-    def write_calendar(self, calendar: Iterable):
-        calendars_list = list(map(lambda x: self._format_datetime(x), sorted(set(self.calendar_list + calendar))))
-        np.savetxt(self.future_path, calendars_list, fmt="%s", encoding="utf-8")
+    def write_calendar(self, calendar: List):
+        calendars_list = list(
+            map(lambda x: self._format_datetime(x, self.freq), sorted(set(self.calendar_list + calendar)))
+        )
+        np.savetxt(self.future_path, X=np.array(calendars_list), fmt="%s", encoding="utf-8")
 
     @abc.abstractmethod
-    def collector(self) -> Iterable[pd.Timestamp]:
+    def collector(self) -> List[pd.Timestamp]:
         """
 
         Returns
@@ -69,7 +75,7 @@ class CollectorFutureCalendar:
 
 
 class CollectorFutureCalendarCN(CollectorFutureCalendar):
-    def collector(self) -> Iterable[pd.Timestamp]:
+    def collector(self) -> List[pd.Timestamp]:
         lg = bs.login()
         if lg.error_code != "0":
             raise ValueError(f"login respond error_msg: {lg.error_msg}")
@@ -89,16 +95,20 @@ class CollectorFutureCalendarCN(CollectorFutureCalendar):
             trading_dates += cal.sessions_in_range(
                 trading_dates[-1] + pd.Timedelta(days=1), pd.Timestamp.now().normalize() + pd.Timedelta(days=60)
             ).to_list()
+        if self.freq == "1min":
+            trading_dates = generate_minutes_calendar_from_daily(trading_dates, self.freq).to_list()
         return trading_dates
 
 
 class CollectorFutureCalendarUS(CollectorFutureCalendar):
-    def collector(self) -> Iterable[pd.Timestamp]:
+    def collector(self) -> List[pd.Timestamp]:
         # TODO: US future calendar
         raise ValueError("Us calendar is not supported")
 
 
-def run(qlib_dir: Union[str, Path], region: str = "cn", start_date: str = None, end_date: str = None):
+def run(
+    qlib_dir: Union[str, Path], region: str = "cn", start_date: str = None, end_date: str = None, freq: str = "day"
+):
     """Collect future calendar(day)
 
     Parameters
@@ -111,6 +121,8 @@ def run(qlib_dir: Union[str, Path], region: str = "cn", start_date: str = None, 
         start date
     end_date
         end date
+    freq
+        freq of calendar
 
     Examples
     -------
@@ -120,7 +132,7 @@ def run(qlib_dir: Union[str, Path], region: str = "cn", start_date: str = None, 
     logger.info(f"collector future calendar: region={region}")
     _cur_module = importlib.import_module("future_calendar_collector")
     _class = getattr(_cur_module, f"CollectorFutureCalendar{region.upper()}")
-    collector = _class(qlib_dir=qlib_dir, start_date=start_date, end_date=end_date)
+    collector = _class(qlib_dir=qlib_dir, start_date=start_date, end_date=end_date, freq=freq)
     collector.write_calendar(collector.collector())
 
 

298637829adee3c6030a91f6fb9d96c98bbbc5ae
booksword <qianyun210603@hotmail.com>
2023-12-07 19:24:52 +0800

    add stop_trading_date to convert instrument info

--- qlib/data/inst_info.py
+++ qlib/data/inst_info.py
@@ -10,10 +10,17 @@ class BaseInstrumentInfo(Serializable):
 
 class ConvertInstrumentInfo(BaseInstrumentInfo):
     def __init__(
-        self, cash_flow_schedule, coupon_schedule, maturity_date, call_date=datetime(2200, 1, 1), principle=100
+        self,
+        cash_flow_schedule,
+        coupon_schedule,
+        maturity_date,
+        call_date=datetime(2200, 1, 1),
+        principle=100,
+        stop_trading_date=datetime(2200, 1, 1),
     ):
         self.cash_flow_schedule = cash_flow_schedule
         self.maturity_date = maturity_date
         self.call_date = call_date
         self.coupon_schedule = coupon_schedule
         self.principle = principle
+        self.stop_trading_date = stop_trading_date
